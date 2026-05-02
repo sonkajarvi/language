3f6a7ff205b59b005a471a56bed5e42dcc6edf1c
sonkajarvi <sonkajarvi@hotmail.com>
2026-05-02 23:00:35 +0300

    parser: fold ast.c into parser.c

--- src/parser.h
+++ src/parser.h
@@ -71,7 +71,7 @@ void skip_whitespace(struct parser *parser);
 struct node *parse_expression(struct parser *parser);
 struct node *parse_statements(struct parser *parser);
 
-struct node *parse_variable_statement(struct parser *parser);
 struct node *parse_if_statement(struct parser *parser);
+struct node *parse_variable_statement(struct parser *parser);
 
 #endif /* __SRC_PARSER_H */
